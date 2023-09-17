f66ee06e06cc88a2e3227ac309b30f16221d7aa0
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-09-17 22:02:20 +0300

    generics

---
+++ src/fatihmas/generics/javacode/JavaGenerics.java
@@ -0,0 +1,18 @@
+package fatihmas.generics.javacode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        //list.add(new BigDecimal(10.5));
+        list.get(0).toUpperCase();
+
+        List anotherList = new ArrayList<>();
+        list.add("goodbye");
+    }
+
+}
